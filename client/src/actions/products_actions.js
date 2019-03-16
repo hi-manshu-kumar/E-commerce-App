b0da3fb0607337f07d258f4fee836d2def7218dd
hi-manshu-kumar <himanshu.kumar394@gmail.com>
2019-03-16 14:31:53 +0530

    logic for filtering of products completed

--- client/src/actions/products_actions.js
+++ client/src/actions/products_actions.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 import {
     GET_PRODUCTS_BY_SELL,
-    GET_PRODUCTS_BY_ARRIVAL
+    GET_PRODUCTS_BY_ARRIVAL,
+    GET_BRANDS,
+    GET_WOODS,
+    GET_PRODUCTS_TO_SHOP
 } from './types';
 
 import { PRODUCT_SERVER } from './../components/utils/misc';
@@ -22,6 +25,54 @@ export function getProductsByArrival(){
 
     return {
         type: GET_PRODUCTS_BY_ARRIVAL,
+        payload: request
+    }
+}
+
+export function getProductsToShop(skip, limit, filters = [], previousState=[]){
+    const data = {
+        limit, 
+        skip,
+        filters
+    }
+
+    const request = axios.post(`${PRODUCT_SERVER}/shop`, data)
+                    .then(response =>{
+                        return {
+                            size: response.data.size,
+                            articles: response.data.articles
+                        }
+                    });
+
+    return {
+        type: GET_PRODUCTS_TO_SHOP,
+        payload: request
+    }
+}
+
+
+/**
+|--------------------------------------------------
+|           CATEGORIES
+|--------------------------------------------------
+*/
+
+export function getBrands(){
+    const request = axios.get(`${PRODUCT_SERVER}/brands`)
+                    .then(response => response.data);
+
+    return {
+        type: GET_BRANDS,
+        payload: request
+    }
+}
+
+export function getWoods(){
+    const request = axios.get(`${PRODUCT_SERVER}/woods`)
+                    .then(response => response.data);
+
+    return {
+        type: GET_WOODS,
         payload: request
     }
 }
