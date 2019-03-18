b36a19a580b11678dfd1612df201a42c3b9281d2
hi-manshu-kumar <himanshu.kumar394@gmail.com>
2019-03-18 18:09:17 +0530

    ADMIN-CLIENT add product

--- client/src/actions/products_actions.js
+++ client/src/actions/products_actions.js
@@ -4,7 +4,9 @@ import {
     GET_PRODUCTS_BY_ARRIVAL,
     GET_BRANDS,
     GET_WOODS,
-    GET_PRODUCTS_TO_SHOP
+    GET_PRODUCTS_TO_SHOP,
+    ADD_PRODUCT,
+    CLEAR_PRODUCT
 } from './types';
 
 import { PRODUCT_SERVER } from './../components/utils/misc';
@@ -53,6 +55,23 @@ export function getProductsToShop(skip, limit, filters = [], previousState=[]){
     return {
         type: GET_PRODUCTS_TO_SHOP,
         payload: request
+    }
+}
+
+export function addProduct(dataToSubmit){
+    const request = axios.post(`${PRODUCT_SERVER}/article`, dataToSubmit)
+                    .then(response => response.data)
+
+    return {
+        type: ADD_PRODUCT,
+        payload: request
+    }
+}
+
+export function clearProduct(){
+    return {
+        type: CLEAR_PRODUCT,
+        payload: ''
     }
 }
 
